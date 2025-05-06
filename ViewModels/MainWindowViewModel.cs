f5c2dd7c35ba01db939d386f5d3256bd77cf9950
DimitarSamarov07 <samarovdimitar@gmail.com>
2025-05-06 23:50:31 +0300

    Demonstrate buttons, text boxes, events and animations.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,10 @@
-﻿namespace file_hasher_async.ViewModels;
+﻿using Avalonia.Interactivity;
+
+namespace file_hasher_async.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public string Greeting { get; } = "Welcome to file-hasher-async!";
+    public string WorkInProgress{ get; } = "Please stand back while we are working on doing something. Come back next week :)";
+    
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using Avalonia.Animation;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace file_hasher_async.Views;
 
@@ -7,5 +9,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void ClickHandler(object? sender, RoutedEventArgs e)
+    {
+        Message.Text = "Hashing....(some day)";
+        var animation = (Animation)this.Resources["EmojiMagic"];
+        animation?.RunAsync(Emoji);
     }
 }
